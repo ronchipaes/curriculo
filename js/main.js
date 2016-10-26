aef5171d0a0999c08f45dd4b2937d172d9e26e8a
ronchipaes <ronchipaes@yahoo.com.br>
2016-10-26 09:27:12 -0200

    Adicionado funções ao js

--- js/main.js
+++ js/main.js
@@ -10,8 +10,77 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 50
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('a.navbar-brand').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+    });
+
+    $(document).ready(function() {
+        $("input").blur(function() {
+            if ($(this).val() == "") {
+                $(this).css({
+                    "border": "1px solid #F00",
+                    "padding": "2px"
+                });
+            }
+        });
+        $("#botao").click(function() {
+            var cont = 0;
+            $("#form input").each(function() {
+                if ($(this).val() == "") {
+                    $(this).css({
+                        "border": "1px solid #F00",
+                        "padding": "2px"
+                    });
+                    cont++;
+                }
+            });
+            $("#form textarea").each(function() {
+                if ($(this).val() == "") {
+                    $(this).css({
+                        "border": "1px solid #F00",
+                        "padding": "2px"
+                    });
+                    cont++;
+                } else {
+                    $(this).css({
+                        "border": "1px solid #ccc",
+                        "padding": "2px"
+                    });
+                }
+            });
+
+            if (cont == 0) {
+                $.ajax({
+                    url: 'http://contato-twsatc.rhcloud.com/contato-api/email',
+                    type: 'post',
+                    dataType: 'json',
+                    data: $('#form').serialize(),
+                    success: function(data) {
+                        if (data) {
+                            alert("Informações enviadas com sucesso...");
+
+                            // limpar os campos....
+                            $("#form input").each(function() {
+                                $(this).val("")
+                            });
+                            $("#form textarea").each(function() {
+                                $(this).val("")
+                            });
+                        } else {
+                            alert("Erro ao enviar...");
+                        }
+                    }
+                });
+            }
+        });
     });
 });
